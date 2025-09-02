cb201fe7aec330f2832dfdb384f7c6891fcf9866
Jacob Qumsiyeh <38016471+Jacob11Q1@users.noreply.github.com>
2025-09-02 21:44:04 +0300

    Delete ninja_gold directory

--- ninja_gold/ninja/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Root route for Ninja Gold page
-    path('process_money/', views.process_money, name='process_money'),  # Process gold
-]
