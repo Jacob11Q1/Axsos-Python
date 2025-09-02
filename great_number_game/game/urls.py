81089862625711f649bad0a93b94c95a77206500
Jacob Qumsiyeh <qumsiyeh37@gmail.com>
2025-09-02 20:19:33 +0300

    Add Ninja Gold assignment inside DJango-Stack/Assignments

--- great_number_game/game/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('guess/', views.guess, name="guess"),
-    path('reset/', views.reset, name="reset"),
-]
