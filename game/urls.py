bbb4dc042d903128b3c2be9b2c1c1bd74a499d22
Jacob Qumsiyeh <38016471+Jacob11Q1@users.noreply.github.com>
2025-09-02 18:55:17 +0300

    Delete game directory

--- game/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name = "index"), # main game page
-    path('process_money/', views.process_money, name = "process_money"), #gold actions
-    path('reset/', views.reset, name = "reset"), # to reset the game
-]
